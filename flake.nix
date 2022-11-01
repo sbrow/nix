b0f602baa80c813cccc8e653ba314dc9d32c311d
Spencer Brower <brower.spencer@gmail.com>
2022-11-01 11:34:16 -0400

    feat: Locked the `nixpkgs` version.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A very basic flake";
 
-  outputs = { self }: {
+  outputs = { nixpkgs, self }: {
     nixosModules = {
       laravel = import ./nixos/modules/web-apps/laravel.nix;
       vagrant = { ... }: { imports = [ ./nixos/modules/virtualisation/vagrant.nix ]; };
