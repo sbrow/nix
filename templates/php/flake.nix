8dc7010df3b819b101c2075fde2c3e06b081581a
Spencer Brower <brower.spencer@gmail.com>
2024-03-08 13:46:32 -0500

    feat(templates/php): Updated template to use `process-compose`.

--- templates/php/flake.nix
+++ templates/php/flake.nix
@@ -3,31 +3,62 @@
 
   inputs = {
     # nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    # dolt.url = "github:sbrow/dolt";
     # phps.url = "github:fossar/nix-phps";
     # phps.inputs.nixpkgs.follows = "nixpkgs";
 
     sbrow.url = "github:sbrow/nix";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
   };
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, /* phps, */ sbrow }:
+  outputs = inputs@{ self, flake-parts, nixpkgs, /* phps, */ process-compose-flake, sbrow }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.process-compose-flake.flakeModule
+      ];
       systems = [ "x86_64-linux" ];
 
-      perSystem = { pkgs, system, ... }:
-        /* let phpPkgs = phps.legacyPackages.${system}; in */ {
-        formatter = pkgs.nixpkgs-fmt;
+      perSystem =
+        { pkgs, system, inputs', ... }:
+        let
+          php = pkgs.php.buildEnv {
+            extensions = ({ enabled, all }: enabled ++ (with all; [
+              xdebug
+            ]));
+            extraConfig = ''
+              ; xdebug 3
+              xdebug.mode=debug
+              xdebug.client_port=9000
 
-        devShells.default = pkgs.mkShell
-          {
-            buildInputs = with pkgs; [
-              caddy
-              foreman
-              php
-              phpPackages.composer
-            ];
+              ; xdebug 2
+              xdebug.remote_enable=1
+            '';
           };
-      };
+        in
+        {
+          formatter = pkgs.nixpkgs-fmt;
+
+          process-compose.default.settings.processes = {
+            web.command = "sudo ${pkgs.caddy}/bin/caddy run";
+            mail.command = "${pkgs.mailhog}/bin/MailHog";
+            php.command = "${php}/bin/php-fpm -F -y php-fpm.conf";
+            # redis.command = "${$pks.redis}/bin/redis-server";
+            # db.command = "${doltPkg}/bin/dolt sql-server --config dolt.yml";
+          };
+
+          devShells.default = pkgs.mkShell
+            {
+              buildInputs = with pkgs; [
+                caddy
+
+                php
+                php.packages.composer
+                php.packages.phpcbf
+                php.packages.phpcs
+              ];
+            };
+        };
     };
 }
